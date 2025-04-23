d5ae6ebd4532c7cf70ab944952da869af0ecadcb
Avain <sports113499@gmail.com>
2025-04-23 23:30:06 +0800

    feat: add vue project with vue-router, pinia, eslint, prettier

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+
+export default defineConfig([
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      // 新增風格規範
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      indent: ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-parens': ['error', 'always'],
+      'no-unused-vars': ['warn'],
+      'no-console': ['warn'],
+    },
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'], // essentail 改為 recommended
+  // skipFormatting,
+]);
