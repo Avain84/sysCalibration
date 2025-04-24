140a83c41cbf0953f8d13e7243c9846705e60967
Avain <sports113499@gmail.com>
2025-04-24 21:27:40 +0800

    feat: add vee-validate settings in main.js

--- src/main.js
+++ src/main.js
@@ -6,13 +6,37 @@ import { createApp } from 'vue';
 import { createBootstrap } from 'bootstrap-vue-next';
 import { createPinia } from 'pinia';
 
+// VeeValidate
+import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate';
+import * as rules from '@vee-validate/rules';
+import { localize, setLocale } from '@vee-validate/i18n';
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
+
 import App from './App.vue';
 import router from './router';
+
+// VeeValidate 規則匯入
+Object.keys(rules).forEach((rule) => {
+  if (rule !== 'all') {
+    defineRule(rule, rules[rule]);
+  }
+});
+
+configure({
+  generateMessage: localize({ zh_TW: zhTW }),
+  validateOnInput: true,
+});
+setLocale('zh_TW');
 
 const app = createApp(App);
 
 app.use(createBootstrap());
 app.use(createPinia());
 app.use(router);
+
+// VeeValidate 標籤設定
+app.component('VField', Field);
+app.component('VForm', Form);
+app.component('ErrorMessage', ErrorMessage);
 
 app.mount('#app');
