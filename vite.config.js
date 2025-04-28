b7595be829774e0534df2726fb8b9435378ea8cb
Avain <sports113499@gmail.com>
2025-04-28 20:44:49 +0800

    feat: add baseurl when production mode

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/sysCalibration/' : '/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+}));
