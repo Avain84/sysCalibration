48601fcf6d0d51be0ee1220c8b14db1d1aaf77a0
Avain <sports113499@gmail.com>
2025-05-11 23:16:40 +0800

    feat: add select company popover, line arrow componenets by current color fill, custom button components

--- src/views/companyManagement/Index.vue
+++ src/views/companyManagement/Index.vue
@@ -1,35 +1,66 @@
 <script setup>
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
-import IconButton from '@/components/buttons/IconButton.vue';
+import CustomButton from '@/components/CustomButton.vue';
+import LineArrow from '@/components/icons/LineArrow.vue';
+import FormInput from '@/components/inputs/FormInput.vue';
 </script>
 
 <template>
   <section class="px-3 pt-12 xl:pt-16 pb-8">
-    <div class="px-3 border-l-4 border-tertiary-30 flex justify-between items-center">
+    <div class="relative px-3 border-l-4 border-tertiary-30 flex justify-between items-center">
       <h2 class="text-h4B xl:text-h3B py-2">校驗商管理</h2>
-      <Popover class="relative">
-        <!-- <PopoverButton>Solutions</PopoverButton> -->
-        <PopoverButton>
-          <IconButton
+      <Popover>
+        <PopoverButton class="focus:outline-none">
+          <CustomButton
+            type="button"
             size="sm"
             role="company"
             :filled="false"
           >
-            <span class="text-tertiary-30">篩選</span>
-            <img
-              src="/src/assets/icons/eye-open.svg"
-              alt=""
-            />
-          </IconButton>
+            <span>篩選</span>
+            <LineArrow />
+          </CustomButton>
         </PopoverButton>
-
-        <PopoverPanel class="absolute z-10 right-0 top-8">
-          <div class="grid grid-cols-2">
-            <a href="/analytics">Analytics</a>
-            <a href="/engagement">Engagement</a>
-            <a href="/security">Security</a>
-            <a href="/integrations">Integrations</a>
-          </div>
+        <PopoverPanel class="absolute z-10 right-0 left-0 top-16 px-3">
+          <VForm
+            class="px-6 py-4 border border-neutral-60 rounded-2xl grid grid-cols-9 gap-x-8 gap-y-3"
+          >
+            <FormInput
+              name="status"
+              label="啟用狀態"
+              id="status"
+              rule="required"
+              class="col-span-9 md:col-span-3"
+            />
+            <FormInput
+              name="status"
+              label="廠商名稱"
+              id="status"
+              rule="required"
+              class="col-span-9 md:col-span-6"
+            />
+            <FormInput
+              name="status"
+              label="廠商統編"
+              id="status"
+              rule="required"
+              class="col-span-9 md:col-span-3"
+            />
+            <FormInput
+              name="status"
+              label="窗口電話"
+              id="status"
+              rule="required"
+              class="col-span-9 md:col-span-3"
+            />
+            <CustomButton
+              size="md"
+              role="company"
+              class="col-span-9 md:col-span-2 md:col-end-10 mb-2 self-end"
+            >
+              搜尋
+            </CustomButton>
+          </VForm>
         </PopoverPanel>
       </Popover>
     </div>
